3ac0d9219e0331e7e10903fa3e927c58407a9213
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-08 10:56:56 -0700

    Proper eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 
-const App: React.FC = () => <div>hiqqq</div>;
+const App: React.FC = () => {
+  return <div>hiqsqqs</div>;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
