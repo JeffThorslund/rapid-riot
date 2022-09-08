21de7294ea7149a8e747c55ecb2e6ca49dcda8df
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-08 09:22:23 -0700

    Remove changes

--- src/database/supabase.test.ts
+++
@@ -1,9 +0,0 @@
-import { supabase } from './supabase'
-
-test('is session active', async () => {
-  await supabase.auth.signOut()
-  const data = await supabase.auth.session()
-  expect(data).toBe(null)
-})
-
-export {}
