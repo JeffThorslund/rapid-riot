4203bc78bbe2c89ac87504f5ea5afa06ebfd35dd
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-08 12:36:30 -0700

    Create a list/map split view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { Grommet } from "grommet";
+import { Grommet, Box } from "grommet";
 import { theme } from "./grommetTheme";
 import { PageHeader } from "./PageHeader";
 import { Map } from "./components/map";
+import { List } from "./components/list";
 
 const App = () => {
   return (
     <Grommet theme={theme}>
       <PageHeader />
-      <Map />
+      <Box direction="row">
+        <Box width={"50%"}>
+          <Map />
+        </Box>
+
+        <Box width={"50%"}>
+          <List />
+        </Box>
+      </Box>
     </Grommet>
   );
 };
---
+++ src/components/list/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Box } from "grommet";
+
+export const List = () => {
+  return <Box>hi</Box>;
+};
