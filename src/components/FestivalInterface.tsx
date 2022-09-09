05cd24060a464f59e7195c6b56859e966925a0e7
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-09 12:55:43 -0700

    Enrich mock data

--- src/components/FestivalInterface.tsx
+++ src/components/FestivalInterface.tsx
@@ -1,16 +1,23 @@
 import { getFestivals } from "../database";
 import { useMap } from "react-map-gl";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "grommet";
 import { MapElement } from "./map";
 import { List } from "./list";
 import { getVisibleFestivalMarkers } from "./_utils/getVisibleFestivalMarkers";
 import { useViewState } from "./_utils/useViewState";
+import { Festival } from "../types";
 
 export const FestivalInterface = () => {
   const mapRef = useMap();
-  const festivals = getFestivals();
   const [viewState, setViewState] = useViewState();
+  const [festivals, setFestivals] = useState<Festival[]>([]);
+
+  useEffect(() => {
+    const festivals = getFestivals();
+
+    setFestivals(festivals);
+  }, []);
 
   return (
     <Box direction="row" height={"500px"}>
