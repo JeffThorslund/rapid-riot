aedb651c150fac3de6454a3e1560c05024d747a7
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-16 20:20:06 -0700

    Clearer header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,10 +1,13 @@
-import { Header } from "grommet";
+import { Box, Header, Text } from "grommet";
 import React from "react";
 
 export function PageHeader() {
   return (
-    <Header background={"dark-1"} pad="small">
-      Rapid Riot
+    <Header background={"light-1"} pad="small">
+      <Box>
+        <Text weight={"bold"}>Rapid Riot</Text>
+      </Box>
+      <Box>Whitewater Festival Database</Box>
     </Header>
   );
 }
