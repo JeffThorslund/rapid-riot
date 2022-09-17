a84f80ff12d7860d87f03f1334846eec339c84aa
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-16 23:35:28 -0700

    Sort festivals based on title

--- src/components/_utils/prepareFestivalData.tsx
+++ src/components/_utils/prepareFestivalData.tsx
@@ -11,9 +11,11 @@ export const prepareFestivalData = (
 };
 
 const filterOutOfBoundsMarkers = (festivals: Festival[], mapRef: MapRef) =>
-  festivals.filter((f) =>
-    mapRef.getMap().getBounds().contains({
-      lat: f.location.coordinates.lat,
-      lng: f.location.coordinates.lng,
-    })
-  );
+  festivals
+    .filter((f) =>
+      mapRef.getMap().getBounds().contains({
+        lat: f.location.coordinates.lat,
+        lng: f.location.coordinates.lng,
+      })
+    )
+    .sort((a, b) => a.title.localeCompare(b.title));
