dd62e0928cfa92e8a8a352f7d351a8fa8d92a70d
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-18 11:22:07 -0700

    Change public html

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,10 @@
 import { Festival } from "../types";
 import { dataProvider } from "./data";
-import { mockDataProvider } from "./mock";
 
 export const getFestivalData = (): Festival[] => {
   if (process.env.NODE_ENV === "production") {
     return dataProvider();
   }
 
-  return mockDataProvider();
+  return dataProvider();
 };
