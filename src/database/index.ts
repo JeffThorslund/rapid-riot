fd6ad0e8f8711717dded71c20549210e496e641a
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-09-20 09:18:44 -0700

    remove mock data

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,9 +2,5 @@ import { Festival } from "../types";
 import { dataProvider } from "./data";
 
 export const getFestivalData = (): Festival[] => {
-  if (process.env.NODE_ENV === "production") {
-    return dataProvider();
-  }
-
   return dataProvider();
 };
--- src/database/mock/date.ts
+++
@@ -1,5 +0,0 @@
-export const getRandomDate = (start: Date, end: Date) => {
-  return new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-};
